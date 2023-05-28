57aa3f27d15504f49cf354abdae5771b7e214ca6
lazynina <lazynina84@gmail.com>
2023-05-28 13:45:57 -0400

    update sign transaction to use deso-protocol library

--- functions/handlers/sign.js
+++ functions/handlers/sign.js
@@ -1,4 +1,4 @@
-const { signTransaction } = require("./signTransaction");
+const { signTx } = require("deso-protocol");
 const { decryptSeedHex } = require("./decryptSeedHex");
 
 exports.signHex = async (req, res) => {
@@ -6,11 +6,12 @@ exports.signHex = async (req, res) => {
   let transactionHex = req.body.transactionHex;
   let encryptedSeedHex = req.body.encryptedSeedHex;
   let hostEncryptionKey = req.body.hostEncryptionKey;
+  let isDerivedKey = req.body.isDerivedKey?.toLowerCase() === "true";
 
   //Decrypt seed hex and use decrypted seed hex to sign transaction
   const seedHex = decryptSeedHex(encryptedSeedHex, hostEncryptionKey);
-  let signedHex = signTransaction(seedHex, transactionHex);
-
+  // Note: if you need to sign a transaction with a derived key,
+  const signedHex = await signTx(transactionHex, seedHex, { isDerivedKey });
   //Return JSON object with signedHex
   return res.status(200).json({
     signedHex,
